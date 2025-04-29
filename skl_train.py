33bbf5ef8d8d18accb5989021551be3ceae194ac
Sarah <74324061+M1N3MY@users.noreply.github.com>
2025-04-29 00:49:55 -0400

    Update skl_train.py
    - Splits the data and allocates for testing (20%) and training (80%) (test_train_split)
    - Dataset is trained using the linear regression model object
    - The mean squared error is calculated by comparing the predicted prices by the actual prices

--- skl_train.py
+++ skl_train.py
@@ -1,49 +1,46 @@
 """
-    The following file trains a model using sklearn
+    The following file trains the housing price prediction dataset using sklearn
+
+    * Model trains and resets each time
 """
-# imports pandas, numpy, and sklearn modules 
+# Imports pandas, numpy, and sklearn modules 
 import pandas as pd
 import numpy as np
 import sklearn as sk
 
-# imports classes and functions from the modules
+# Imports classes and functions from the modules
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error
 
-# allows the full dataframe to be displayed 
+# Allows the full dataframe to be displayed 
 pd.set_option('display.max_columns', None)
 
-# loads housing price dataset into pandas dataframe (dataset)
+# Loads housing price dataset into pandas dataframe (dataset)
 dataset = pd.read_csv('housing_price_dataset.csv')
 
-print(dataset.shape)
+print(f"rows and columns: {dataset.shape}")
 
-# prints the entire dataframe (house id, square feet, bedroom, bathroom, neighborhood, year built, price)
-#print(dataset.to_string())
+'''Prints the entire dataframe (house id, square feet, bedroom, bathroom, neighborhood, year built, price)
+   print(dataset.to_string())'''
 
-# chooses the features and target 
+# Chooses the features and target 
 features = ['SquareFeet', 'Bedrooms', 'Bathrooms', 'Neighborhood', 'YearBuilt']
 target = ['Price']
 X = dataset[features]
 y = dataset[target]
 
-# transforms the data in the neighborhood column as binary values
-dummies = pd.get_dummies(X, columns=['Neighborhood'], drop_first=False)
-print(dummies.columns)
-print(dummies)
+# Transforms the data in the neighborhood column as binary values
+x_encoded = pd.get_dummies(X, columns=['Neighborhood'], drop_first=False)
+print(f"new columns after pd.get_dummies: {x_encoded.columns}")
 
-print(dataset.corr()['price'].sort_values(ascending=False))
+# Splits the data for testing (20%) and training (80%)
+X_train, X_test, y_train, y_test = train_test_split(x_encoded, y, test_size = 0.2, random_state = 42)
 
-"""
-mean squared error - loss, dividing by number of data points (n) 
-
-generate linear regression model sci kit learn
-
-improve error rate
-
-normalization
-
-"""
-
+# Trains the dataset using linear regression and compares results using mean squared error
+model = LinearRegression() # linear regression model object created
+model.fit(X_train, y_train) # trains the data using the training data
+predictions = model.predict(X_test) # predicts prices using the testing data
+skl_mse = mean_squared_error(y_test, predictions) # compares predicted prices to actual prices
+print(f"mean squared error: {skl_mse}") 
